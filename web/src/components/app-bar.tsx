ddf199f1907f1ef7cdaacd4465552554d7273e9a
Acho Arnold <arnold@ndolestudio.com>
2025-02-25 15:49:50 +0200

    fix: Add button to change the project ID

--- web/src/components/app-bar.tsx
+++ web/src/components/app-bar.tsx
@@ -9,6 +9,7 @@ import {
   ActionList,
   Avatar,
   Box,
+  Text,
 } from "@primer/react";
 import { Heading } from "@primer/react";
 import Logo from "@/app/logo.svg";
@@ -16,15 +17,44 @@ import {
   SignInIcon,
   SignOutIcon,
   PersonFillIcon,
+  PlusIcon,
 } from "@primer/octicons-react";
 import { usePathname, useRouter } from "next/navigation";
 import { useClerk, useUser } from "@clerk/nextjs";
+import { useAppStore } from "@/store/provider";
+import { EntitiesProject } from "@/api/model";
+import { useEffect, useState } from "react";
 
 export function AppBar() {
   const router = useRouter();
   const pathname = usePathname();
   const user = useUser();
   const { signOut } = useClerk();
+  const { indexProjects } = useAppStore((state) => state);
+  const [projects, setProjects] = useState<Array<EntitiesProject>>([]);
+  const [activeProject, setActiveProject] = useState<EntitiesProject | null>(
+    null,
+  );
+
+  useEffect(() => {
+    indexProjects().then((projects: Array<EntitiesProject>) => {
+      setProjects(projects);
+    });
+  }, [indexProjects]);
+
+  useEffect(() => {
+    projects.forEach((project: EntitiesProject) => {
+      if (pathname.startsWith(`/projects/${project.id}`)) {
+        setActiveProject(project);
+      }
+    });
+    if (
+      !pathname.startsWith("/projects") ||
+      pathname.startsWith("/projects/create")
+    ) {
+      setActiveProject(null);
+    }
+  }, [pathname, projects]);
 
   return (
     <Header>
@@ -34,6 +64,41 @@ export function AppBar() {
           httpmock
         </Heading>
       </Header.Item>
+      {activeProject && (
+        <ActionMenu>
+          <ActionMenu.Button size={"large"} sx={{ mt: 2 }}>
+            <Text variant={"large"} weight={"semibold"}>
+              {activeProject.name}
+            </Text>
+          </ActionMenu.Button>
+          <ActionMenu.Overlay width="auto">
+            <ActionList selectionVariant="single">
+              {projects.map((project: EntitiesProject) => (
+                <ActionList.Item
+                  key={project.id}
+                  onSelect={() => router.push(`/projects/${project.id}`)}
+                  selected={project.id === activeProject?.id}
+                >
+                  <Text weight={"semibold"}>{project.name}</Text>
+                </ActionList.Item>
+              ))}
+            </ActionList>
+            <ActionList.Group>
+              <ActionList.Divider />
+              <ActionList.Item>
+                <Button
+                  onClick={() => router.push("/projects/create")}
+                  variant={"primary"}
+                  block={true}
+                  leadingVisual={PlusIcon}
+                >
+                  Create Project
+                </Button>
+              </ActionList.Item>
+            </ActionList.Group>
+          </ActionMenu.Overlay>
+        </ActionMenu>
+      )}
       <Box sx={{ flex: "auto" }}></Box>
       <Header.Item>
         {pathname === "/sign-up" && (
